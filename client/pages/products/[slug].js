920a06db18f1f0e6d52f845fa3304052755441e1
benlittlenz <benlittle53@gmail.com>
2021-04-25 12:32:01 +1200

    added product design page

--- client/pages/products/[slug].js
+++ client/pages/products/[slug].js
@@ -3,16 +3,48 @@ import axios from 'axios';
 import { useRouter } from 'next/router'
 
 import ProductDetails from '../../components/ProductDetails';
+import Navigation from '../../components/Navigation';
 
-export default function Home({product}) {
+export default function Home({ product }) {
     console.log('hey')
     console.log(product)
     return (
-        <div>
-            dsffdsfdsd
-            Name: {product.data.name}
-            Slug: {product.data.slug}
-        </div>
+        <>
+        <Navigation />
+            <section class="text-gray-700 body-font overflow-hidden bg-white">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+                        <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src="https://www.whitmorerarebooks.com/pictures/medium/2465.jpg" />
+                        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                            <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+                            <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{product.data.name}</h1>
+                            <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+                            <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                                <div class="flex items-center">
+                                    <span class="mr-3">Size</span>
+                                    <div class="relative">
+                                        <select class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
+                                            <option>Regular</option>
+                                            <option>Large</option>
+                                        </select>
+                                        <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+                                                <path d="M6 9l6 6 6-6"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
+                                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+
     )
 }
 
